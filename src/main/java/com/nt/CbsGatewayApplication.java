78f836c2f257022b4ddee861f39c9b5fc092e1dd
unknown <manishjangir8080@gmail.com>
2022-05-04 19:46:22 +0530

    test 2

---
+++ src/main/java/com/nt/CbsGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsGatewayApplication {
+
+	public static void main(String[] args) {
+		System.out.print("tes  t");
+		SpringApplication.run(CbsGatewayApplication.class, args);
+	}
+
+}
